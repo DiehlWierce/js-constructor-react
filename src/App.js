61e23f88586a4687d359dccc2497e79bd5df2479
Diehl Wierce <johnbrotherwood@gmail.com>
2020-12-17 11:48:44 +0300

    Commit #2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {Component} from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 
-class App extends React.Component {
+
+class App extends Component {
   constructor() {
     super();
     this.state = "App";
